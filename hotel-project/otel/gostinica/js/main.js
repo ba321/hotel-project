6a211fa786aee7b7872eea2b7a726f70191bc79d
expstry <150355360+expstry@users.noreply.github.com>
2023-11-30 12:44:28 +0300

    Add files via upload

---
+++ hotel-project/otel/gostinica/js/main.js
@@ -0,0 +1,94 @@
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Прелоадер
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Фоновый набор
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Меню Offcanvas
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Модель поиска
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+
+    /*------------------
+        Герой слайдер
+    --------------------*/
+   $(".hero-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false
+    });
+
+    /*------------------------
+		Слайдер отзывов
+    ----------------------- */
+    $(".testimonial-slider").owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        smartSpeed: 1200,
+        nav: true,
+        navText: ["<i class='arrow_left'></i>", "<i class='arrow_right'></i>"]
+    });
+
+    /*------------------
+        Великолепное всплывающее окно
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+		Выбор даты
+	--------------------*/
+    $(".date-input").datepicker({
+        minDate: 0,
+        dateFormat: 'dd MM, yy'
+    });
+
+    /*------------------
+		Хороший выбор
+	--------------------*/
+    $("select").niceSelect();
+
+})(jQuery);
